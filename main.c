2b543249e1a9389de7bef9d2321ed47574681796
Christopher Buck <cmbuck@vt.edu>
2013-06-09 12:25:16 -0400

    Code works. Uses timer 1 and ISR

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
 
 #define F_CPU 1000000UL  // 1 MHz
 
@@ -10,30 +11,27 @@
 #define bit_write(c,p,m) (c ? bit_set(p,m) : bit_clear(p,m)) 
 #define BIT(x) (0x01 << (x))
 
+ISR(TIMER1_COMPA_vect)
+{
+	PORTB ^= (1 << 0);  //toggle LED
+}
 
 int main(void)
 {
 	DDRB |= BIT(0);
 	
-	TCCR1B |= (1 << CS10 ); // Set up timer
+	TCCR1B |= (1 << WGM12); //enable CTC mode
+	
+	OCR1A = 488; //about 2 Hz with 1024 prescale
+	
+	TCCR1B |= (1 << CS10) | (1 << CS12); // Set up timer with 1024 prescale
+	
+	TIMSK1 |= (1 << OCIE1A); //enable timer1 CTC interrupt
+	sei();			//enable global interrupts
 	
 	while (1)
 	{
-		/**
-		 *bit_set( PORTB, BIT(0) );
-		 *_delay_ms(100);
-		 *bit_clr( PORTB, BIT(0) );
-		 *_delay_ms(100);
-		 */
-
-		if (TCNT1 > 49999)
-		{
-			bit_flip( PORTB, BIT(0) );
-			
-			TCNT1 = 0;
-		}
-		
-		
+		//blinking is handled with ISR	
 	}
 	
 }
